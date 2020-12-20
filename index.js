ef33380606df1e79039efcf0f0aa7e5e9b345709
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-20 22:38:27 +0530

    Connected To mLab

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const mongoose = require("mongoose")
+
+
 const app = express();
+
+mongoose.connect("mongodb+srv://books_user:books_password@cluster0.cbckc.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {useUnifiedTopology: true}, () => {
+        console.log("DB Is Connected");
+    })
 
 app.get('/', (req, res) => {
     res.send("Hello From Books Microservice");
@@ -7,5 +15,5 @@ app.get('/', (req, res) => {
 
 
 app.listen(4000, () => {
-   console.log("Up & Running! Books Microservice");
+    console.log("Up & Running! Books Microservice");
 });
